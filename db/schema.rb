f4a5ea1507047da14a87bf511ce8c4c0c60dafd2
PierreOssun <pierre.giraud@mailfence.com>
2018-03-13 09:51:00 +0100

    heroku first commit

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20180312151613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.integer "postalcode"
+    t.integer "Postalcode"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
