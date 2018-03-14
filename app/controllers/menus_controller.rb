ca1c11ad3112d64453517a95e3d86d28b23f1b92
Matthieu <matthieu.colombert@gmail.com>
2018-03-14 14:03:51 +0100

    controlleur dishs ok

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -9,22 +9,27 @@ class MenusController < ApplicationController
   	@menu.save
   end
   def show
-  	@menu = Menu.find(params[:id])
+  	@menu = current_menu
+  	@dishes = @menu.dishes
   end
 
   def edit
-  	@menu = Menu.find(params[:id])
+  	@menu = current_menu
+  	@dishes = @menu.dishes
   end
 
   def update
-  	@menu = Menu.find(params[:id])
+  	@menu = current_menu
   	if @menu.update(params.require(:menu).permit(:description, :image))
       redirect_to @menu
     end
   end
 
+  
   private #on ne récupère de la view que les données qui nous intéressent (anonyme_user et content) - placé ici dans le script par convention
     def menu_params
       params.permit(:description, :image)
     end
+
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(version: 20180313215916) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.integer "Postalcode"
+    t.integer "postalcode"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
