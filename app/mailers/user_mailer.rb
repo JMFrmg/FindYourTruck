3103c97378fce799019eeda63990c68057dda608
PierreOssun <pierre.giraud@mailfence.com>
2018-03-26 14:34:03 +0200

    herokus

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 	def buy_email(user, cart, sum, foodtruck)
 		headers['X-MJ-CustomID'] = 'rubyPR_Test_ID_1469790724'
 		headers['X-MJ-EventPayload'] = 'rubyPR_Test_Payload'
-		headers['X-MJ-TemplateLanguage'] = 'true'	
+		headers['X-MJ-TemplateLanguage'] = 'true'
     	@user, @cart, @sum, @foodtruck = user, cart, sum, foodtruck
         Dotenv.load
 		mail(
-    		delivery_method_options: { api_key: ENV['MAILJET_API_KEY'], secret_key: ENV['MAILJET_SECRET_KEY'], port: '25' },
+    		delivery_method_options: { api_key: '2f48b7b5331c5aaae12ea4e8b073c22f', secret_key: 'f20ab8630b031fc7fbd7ea6b64fc93e1', port: '25' },
     		from: "findyourtruck@gmx.fr",
     		to: @user.email,
     		subject: 'Détails de votre commande')
@@ -20,7 +20,7 @@ class UserMailer < ApplicationMailer
         @foodtrucker = foodtrucker
         Dotenv.load
         mail(
-            delivery_method_options: { api_key: ENV['MAILJET_API_KEY'], secret_key: ENV['MAILJET_SECRET_KEY'], port: '25' },
+            delivery_method_options: { api_key: '2f48b7b5331c5aaae12ea4e8b073c22f', secret_key: 'f20ab8630b031fc7fbd7ea6b64fc93e1', port: '25' },
             from: "findyourtruck@gmx.fr",
             to: follower.email,
             subject: "Un de vos foodtrucks préférés est en ligne!")
